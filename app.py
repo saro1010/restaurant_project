218d6d4907202532e2173a46a9d60e77bae2e3d3
saro <hoseinisaroo@gmail.com>
2025-12-11 20:42:26 +0330

    code skeleton created

---
+++ app.py
@@ -0,0 +1,69 @@
+import psycopg2
+from psycopg2 import sql
+
+
+class Database:
+    def __init__(self):
+        self.dbname = "restaurant_db"
+        self.user = "postgres"
+        self.password = "YOUR_PASSWORD"
+        self.host = "localhost"
+
+    def connect(self):
+        pass  
+
+
+class MenuManager:
+    def __init__(self, db):
+        self.db = db
+
+    def add_item(self):
+        pass
+
+    def show_menu(self):
+        pass
+
+    def update_item(self):
+        pass
+
+    def delete_item(self):
+        pass
+
+
+class TableManager:
+    def __init__(self, db):
+        self.db = db
+
+    def add_table(self):
+        pass
+
+    def view_tables(self):
+        pass
+
+    def update_table_status(self):
+        pass
+
+    def delete_table(self):
+        pass
+    
+
+class RestaurantApp:
+    def __init__(self):
+        self.db = Database()
+        self.menu_manager = MenuManager(self.db)
+        self.table_manager = TableManager(self.db)
+        self.order_manager = OrderManager(self.db)
+
+    def main_menu(self):
+        while True:
+            print("\n----- Restaurant Manager -----")
+            print("1. Menu Management")
+            print("2. Table Management")
+            print("3. Order Management")
+            print("4. Exit")
+            choice = input("Choose an option: ")
+
+            pass  
+
+app = RestaurantApp()
+app.main_menu()
